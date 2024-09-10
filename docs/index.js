a453ff128f3b7c918eb37a1b11f776d388087aec
Gaby Goldman <82763943+GabyGold67@users.noreply.github.com>
2024-09-10 13:26:47 +0300

    README.md minor  adjustment

--- docs/index.js
+++ docs/index.js
@@ -2,94 +2,94 @@ var index =
 [
     [ "An ESP32-RTOS Arduino library that builds switch mechanisms replacements out of simple push buttons.", "index.html#an-esp32-rtos-arduino-library-that-builds-switch-mechanisms-replacements-out-of-simple-push-buttons", null ],
     [ "The library implements the following switches mechanisms:", "index.html#the-library-implements-the-following-switches-mechanisms", null ],
-    [ "ButtonToSwitch for ESP32 Library Complete Documentation Here!", "index.html#buttontoswitch-for-esp32-library-complete-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32", [
+    [ "For ButtonToSwitch for ESP32 Library Complete Documentation Click Here!", "index.html#for-buttontoswitch-for-esp32-library-complete-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32", [
       [ "The embedded behavior also includes logical replacements for temporary or extraordinary responses tha...", "index.html#the-embedded-behavior-also-includes-logical-replacements-for-temporary-or-extraordinary-responses-that-usually-need-external-hardware-modifications-in-electromechanical-switches", null ]
     ] ],
     [ "DbncdMPBttn class", "index.html#dbncdmpbttn-class", [
       [ "Included Methods for DbncdMPBttn class", "index.html#included-methods-for-dbncdmpbttn-class", null ],
       [ "## Methods definition and use description", "index.html#autotoc_md-methods-definition-and-use-description", null ]
     ] ],
-    [ "DbncdMPBttn class Members Documentation Here!", "index.html#dbncdmpbttn-class-members-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32class_dbncd_m_p_bttn-membershtml", [
-      [ "ButtonToSwitch for ESP32 Library Complete Documentation Here!", "index.html#buttontoswitch-for-esp32-library-complete-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32-1", null ]
+    [ "For DbncdMPBttn class Members Documentation Click Here!", "index.html#for-dbncdmpbttn-class-members-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32class_dbncd_m_p_bttn-membershtml", [
+      [ "For ButtonToSwitch for ESP32 Library Complete Documentation Click Here!", "index.html#for-buttontoswitch-for-esp32-library-complete-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32-1", null ]
     ] ],
     [ "DbncdDlydMPBttn class", "index.html#dbncddlydmpbttn-class", null ],
     [ "Added or modified Methods for DbncdDlydMPBttn class", "index.html#added-or-modified-methods-for-dbncddlydmpbttn-class", [
       [ "## Methods definition and use description", "index.html#autotoc_md-methods-definition-and-use-description-1", null ]
     ] ],
-    [ "DbncdDlydMPBttn class Members Documentation Here!", "index.html#dbncddlydmpbttn-class-members-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32class_dbncd_dlyd_m_p_bttn-membershtml", [
-      [ "ButtonToSwitch for ESP32 Library Complete Documentation Here!", "index.html#buttontoswitch-for-esp32-library-complete-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32-2", null ]
+    [ "For DbncdDlydMPBttn class Members Documentation Click Here!", "index.html#for-dbncddlydmpbttn-class-members-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32class_dbncd_dlyd_m_p_bttn-membershtml", [
+      [ "For ButtonToSwitch for ESP32 Library Complete Documentation Click Here!", "index.html#for-buttontoswitch-for-esp32-library-complete-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32-2", null ]
     ] ],
     [ "LtchMPBttn class", "index.html#ltchmpbttn-class", [
       [ "Added or Modified Methods for LtchMPBttn class", "index.html#added-or-modified-methods-for-ltchmpbttn-class", null ],
       [ "## Methods definition and use description", "index.html#autotoc_md-methods-definition-and-use-description-2", null ]
     ] ],
-    [ "LtchdMPBttn Subclasses Common Members Documentation Here!", "index.html#ltchdmpbttn-subclasses-common-members-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32class_ltch_m_p_bttn-membershtml", [
-      [ "ButtonToSwitch for ESP32 Library Complete Documentation Here!", "index.html#buttontoswitch-for-esp32-library-complete-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32-3", null ]
+    [ "For LtchdMPBttn Subclasses Common Members Documentation Click Here!", "index.html#for-ltchdmpbttn-subclasses-common-members-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32class_ltch_m_p_bttn-membershtml", [
+      [ "For ButtonToSwitch for ESP32 Library Complete Documentation Click Here!", "index.html#for-buttontoswitch-for-esp32-library-complete-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32-3", null ]
     ] ],
     [ "TgglLtchMPBttn class", "index.html#tgglltchmpbttn-class", [
       [ "Added or Modified Methods for LtchMPBttn class", "index.html#added-or-modified-methods-for-ltchmpbttn-class-1", null ],
       [ "## Methods definition and use description", "index.html#autotoc_md-methods-definition-and-use-description-3", null ]
     ] ],
-    [ "TgglLtchdMPBttn Subclasses Common Members Documentation Here!", "index.html#tgglltchdmpbttn-subclasses-common-members-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32class_tggl_ltch_m_p_bttn-membershtml", [
-      [ "ButtonToSwitch for ESP32 Library Complete Documentation Here!", "index.html#buttontoswitch-for-esp32-library-complete-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32-4", null ]
+    [ "For TgglLtchdMPBttn Subclasses Common Members Documentation Click Here!", "index.html#for-tgglltchdmpbttn-subclasses-common-members-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32class_tggl_ltch_m_p_bttn-membershtml", [
+      [ "For ButtonToSwitch for ESP32 Library Complete Documentation Click Here!", "index.html#for-buttontoswitch-for-esp32-library-complete-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32-4", null ]
     ] ],
     [ "TmLtchMPBttn class", "index.html#tmltchmpbttn-class", [
       [ "Added or Modified Methods for TmLtchMPBttn class", "index.html#added-or-modified-methods-for-tmltchmpbttn-class", null ],
       [ "## Methods definition and use description", "index.html#autotoc_md-methods-definition-and-use-description-4", null ]
     ] ],
-    [ "TmLtchdMPBttn Subclasses Common Members Documentation Here!", "index.html#tmltchdmpbttn-subclasses-common-members-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32class_tm_ltch_m_p_bttn-membershtml", [
-      [ "ButtonToSwitch for ESP32 Library Complete Documentation Here!", "index.html#buttontoswitch-for-esp32-library-complete-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32-5", null ]
+    [ "For TmLtchdMPBttn Subclasses Common Members Documentation Click Here!", "index.html#for-tmltchdmpbttn-subclasses-common-members-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32class_tm_ltch_m_p_bttn-membershtml", [
+      [ "For ButtonToSwitch for ESP32 Library Complete Documentation Click Here!", "index.html#for-buttontoswitch-for-esp32-library-complete-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32-5", null ]
     ] ],
     [ "HntdTmLtchMPBttn class", "index.html#hntdtmltchmpbttn-class", [
       [ "Added or Modified Methods for HntdTmLtchMPBttn class", "index.html#added-or-modified-methods-for-hntdtmltchmpbttn-class", null ],
       [ "## Methods definition and use description", "index.html#autotoc_md-methods-definition-and-use-description-5", null ]
     ] ],
-    [ "HntdTmLtchMPBttn class Members Documentation Here!", "index.html#hntdtmltchmpbttn-class-members-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32class_hntd_tm_ltch_m_p_bttn-membershtml", [
-      [ "ButtonToSwitch for ESP32 Library Complete Documentation Here!", "index.html#buttontoswitch-for-esp32-library-complete-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32-6", null ]
+    [ "For HntdTmLtchMPBttn class Members Documentation Click Here!", "index.html#for-hntdtmltchmpbttn-class-members-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32class_hntd_tm_ltch_m_p_bttn-membershtml", [
+      [ "For ButtonToSwitch for ESP32 Library Complete Documentation Click Here!", "index.html#for-buttontoswitch-for-esp32-library-complete-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32-6", null ]
     ] ],
     [ "XtrnUnLtchMPBttn class", "index.html#xtrnunltchmpbttn-class", [
       [ "Added or Modified Methods for XtrnUnltchMPBttn class", "index.html#added-or-modified-methods-for-xtrnunltchmpbttn-class", null ],
       [ "## Methods definition and use description", "index.html#autotoc_md-methods-definition-and-use-description-6", null ]
     ] ],
-    [ "XtrnUnltchMPBttn class Members Documentation Here!", "index.html#xtrnunltchmpbttn-class-members-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32class_xtrn_unltch_m_p_bttn-membershtml", [
-      [ "ButtonToSwitch for ESP32 Library Complete Documentation Here!", "index.html#buttontoswitch-for-esp32-library-complete-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32-7", null ],
+    [ "For XtrnUnltchMPBttn class Members Documentation Click Here!", "index.html#for-xtrnunltchmpbttn-class-members-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32class_xtrn_unltch_m_p_bttn-membershtml", [
+      [ "For ButtonToSwitch for ESP32 Library Complete Documentation Click Here!", "index.html#for-buttontoswitch-for-esp32-library-complete-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32-7", null ],
       [ "# DblActnLtchMPBttn class", "index.html#autotoc_md-dblactnltchmpbttn-class--", null ],
       [ "Added or Modified Methods for DblActnLtchMPBttn class", "index.html#added-or-modified-methods-for-dblactnltchmpbttn-class", null ],
       [ "## Methods definition and use description", "index.html#autotoc_md-methods-definition-and-use-description-7", null ]
     ] ],
-    [ "DblActnLtchMPBttn Subclasses Common Members Documentation Here!", "index.html#dblactnltchmpbttn-subclasses-common-members-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32class_dbl_actn_ltch_m_p_bttn-membershtml", [
-      [ "ButtonToSwitch for ESP32 Library Complete Documentation Here!", "index.html#buttontoswitch-for-esp32-library-complete-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32-8", null ],
+    [ "For DblActnLtchMPBttn Subclasses Common Members Documentation Click Here!", "index.html#for-dblactnltchmpbttn-subclasses-common-members-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32class_dbl_actn_ltch_m_p_bttn-membershtml", [
+      [ "For ButtonToSwitch for ESP32 Library Complete Documentation Click Here!", "index.html#for-buttontoswitch-for-esp32-library-complete-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32-8", null ],
       [ "# DDlydDALtchMPBttn class", "index.html#autotoc_md-ddlyddaltchmpbttn-class", null ],
       [ "Added or Modified Methods for DDlydDALtchMPBttn class", "index.html#added-or-modified-methods-for-ddlyddaltchmpbttn-class", null ],
       [ "## Methods definition and use description", "index.html#autotoc_md-methods-definition-and-use-description-8", null ]
     ] ],
-    [ "DDlydDALtchMPBttn Members Documentation Here!", "index.html#ddlyddaltchmpbttn-members-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32class_d_dlyd_d_a_ltch_m_p_bttn-membershtml", [
-      [ "ButtonToSwitch for ESP32 Library Complete Documentation Here!", "index.html#buttontoswitch-for-esp32-library-complete-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32-9", null ],
+    [ "For DDlydDALtchMPBttn Members Documentation Click Here!", "index.html#for-ddlyddaltchmpbttn-members-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32class_d_dlyd_d_a_ltch_m_p_bttn-membershtml", [
+      [ "For ButtonToSwitch for ESP32 Library Complete Documentation Click Here!", "index.html#for-buttontoswitch-for-esp32-library-complete-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32-9", null ],
       [ "# SldrDALtchMPBttn class", "index.html#autotoc_md-sldrdaltchmpbttn-class", null ],
       [ "Added or Modified Methods for SldrDALtchMPBttn class", "index.html#added-or-modified-methods-for-sldrdaltchmpbttn-class", null ],
       [ "## Methods definition and use description", "index.html#autotoc_md-methods-definition-and-use-description-9", null ]
     ] ],
-    [ "SldrDALtchMPBttn Members Documentation Here!", "index.html#sldrdaltchmpbttn-members-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32class_sldr_d_a_ltch_m_p_bttn-membershtml", [
-      [ "ButtonToSwitch for ESP32 Library Complete Documentation Here!", "index.html#buttontoswitch-for-esp32-library-complete-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32-10", null ],
+    [ "For SldrDALtchMPBttn Members Documentation Click Here!", "index.html#for-sldrdaltchmpbttn-members-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32class_sldr_d_a_ltch_m_p_bttn-membershtml", [
+      [ "For ButtonToSwitch for ESP32 Library Complete Documentation Click Here!", "index.html#for-buttontoswitch-for-esp32-library-complete-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32-10", null ],
       [ "# VdblMPBttn class", "index.html#autotoc_md-vdblmpbttn-class", null ],
       [ "Added or Modified Methods for VdblMPBttn class", "index.html#added-or-modified-methods-for-vdblmpbttn-class", null ],
       [ "## Methods definition and use description", "index.html#autotoc_md-methods-definition-and-use-description-10", null ]
     ] ],
-    [ "VdblMPBttn Subclasses Common Members Documentation Here!", "index.html#vdblmpbttn-subclasses-common-members-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32class_vdbl_m_p_bttn-membershtml", [
-      [ "ButtonToSwitch for ESP32 Library Complete Documentation Here!", "index.html#buttontoswitch-for-esp32-library-complete-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32-11", null ],
+    [ "For VdblMPBttn Subclasses Common Members Documentation Click Here!", "index.html#for-vdblmpbttn-subclasses-common-members-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32class_vdbl_m_p_bttn-membershtml", [
+      [ "For ButtonToSwitch for ESP32 Library Complete Documentation Click Here!", "index.html#for-buttontoswitch-for-esp32-library-complete-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32-11", null ],
       [ "# TmVdblMPBttn class", "index.html#autotoc_md-tmvdblmpbttn-class--", null ]
     ] ],
     [ "Added Methods for TmVdblMPBttn class", "index.html#added-methods-for-tmvdblmpbttn-class", [
       [ "## Methods definition and use description", "index.html#autotoc_md-methods-definition-and-use-description-11", null ]
     ] ],
-    [ "TmVdblMPBttn Members Documentation Here!", "index.html#tmvdblmpbttn-members-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32class_tm_vdbl_m_p_bttn-membershtml", [
-      [ "ButtonToSwitch for ESP32 Library Complete Documentation Here!", "index.html#buttontoswitch-for-esp32-library-complete-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32-12", null ],
+    [ "For TmVdblMPBttn Members Documentation Click Here!", "index.html#for-tmvdblmpbttn-members-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32class_tm_vdbl_m_p_bttn-membershtml", [
+      [ "For ButtonToSwitch for ESP32 Library Complete Documentation Click Here!", "index.html#for-buttontoswitch-for-esp32-library-complete-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32-12", null ],
       [ "# SnglSrvcVdblMPBttn class", "index.html#autotoc_md-snglsrvcvdblmpbttn-class", null ]
     ] ],
     [ "Added Methods for TmVdblMPBttn class", "index.html#added-methods-for-tmvdblmpbttn-class-1", [
       [ "## Methods definition and use description", "index.html#autotoc_md-methods-definition-and-use-description-12", null ]
     ] ],
-    [ "SnglSrvcVdblMPBttn Members Documentation Here!", "index.html#snglsrvcvdblmpbttn-members-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32class_sngl_srvc_vdbl_m_p_bttn-membershtml", [
-      [ "ButtonToSwitch for ESP32 Library Complete Documentation Here!", "index.html#buttontoswitch-for-esp32-library-complete-documentation-herehttpsgabygold67githubiobuttontoswitch_esp32-13", null ]
+    [ "For SnglSrvcVdblMPBttn Members Documentation Click Here!", "index.html#for-snglsrvcvdblmpbttn-members-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32class_sngl_srvc_vdbl_m_p_bttn-membershtml", [
+      [ "For ButtonToSwitch for ESP32 Library Complete Documentation Click Here!", "index.html#for-buttontoswitch-for-esp32-library-complete-documentation-click-herehttpsgabygold67githubiobuttontoswitch_esp32-13", null ]
     ] ]
 ];
