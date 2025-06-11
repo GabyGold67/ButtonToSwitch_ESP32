3e7c6df9ff961ab9f7cae58c76495ce0639a6e80
Gaby Goldman <82763943+GabyGold67@users.noreply.github.com>
2025-06-11 09:23:03 +0300

    v4.4.1 RC00

---
+++ docs/search/pages_d.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['pin_20voltage_0',['- &lt;em&gt;Stop checking the input pin voltage.&lt;/em&gt;',['../C:/Users/DevPadawan/Documents/My Projects/Training Projects/LibsDev_ESP32/lib/ButtonToSwitch_ESP32/README.md#autotoc_md--_stop-checking-the-input-pin-voltage_',1,'']]],
+  ['pretty_20simple_3a_1',['The driving concept is pretty simple:',['../C:/Users/DevPadawan/Documents/My Projects/Training Projects/LibsDev_ESP32/lib/ButtonToSwitch_ESP32/README.md#the-driving-concept-is-pretty-simple',1,'']]],
+  ['push_20buttons_2',['An ESP32-RTOS Arduino library that builds switch mechanisms replacements out of simple push buttons.',['../C:/Users/DevPadawan/Documents/My Projects/Training Projects/LibsDev_ESP32/lib/ButtonToSwitch_ESP32/README.md#an-esp32-rtos-arduino-library-that-builds-switch-mechanisms-replacements-out-of-simple-push-buttons',1,'']]]
+];
