9470b5f98bdb17e286f064619c1897b56a70c810
Gaby Goldman <82763943+GabyGold67@users.noreply.github.com>
2025-07-30 15:36:26 +0300

    Code status as of 2025-07-31 15-36-00

--- examples/09_TmVdblMPBttn_1b/09_TmVdblMPBttn_1b.ino
+++ examples/09_TmVdblMPBttn_1b/09_TmVdblMPBttn_1b.ino
@@ -3,17 +3,20 @@
   * @file	: 09_TmVdblMPBttn_1b.ino
   * @brief  : Example for the ButtonToSwitch_ESP32 library TmVdblMPBttn class
   *
-  *   Framework: Arduino
-  *   Platform: ESP32
+  * Repository: https://github.com/GabyGold67/ButtonToSwitch_ESP32
+  * WOKWI simulation URL: https://wokwi.com/projects/437897959674557441
   * 
-  * The example instantiates a TmVdblMPBttn object using:
+  * Framework: Arduino
+  * Platform: ESP32
+  * 
+  * @details The example instantiates a TmVdblMPBttn object using:
   * 	- 1 push button between GND and dmpbSwitchPin
   * 	- 1 led with it's corresponding resistor between GND and dmpbLoadPin
   *   - 1 led with it's corresponding resistor between GND and tvlmpbLoadPin
   * 	- 1 led with it's corresponding resistor between GND and dmpbIsDisabledPin
   *
-  * ### This example doesn't create extra Tasks:
-  * ### This example creates a software timer
+  * - This example doesn't create extra Tasks:
+  * - This example creates a software timer
   *
   * This simple example instantiates the TmVdblMPBttn object in the setup(),
   * and uses the default `loop ()` (loop() is the loopTask() disguised
@@ -26,18 +29,24 @@
   *
   * A software timer is created so that it periodically toggles the isEnabled attribute flag
   * value, showing the behavior of the instantiated object when enabled and when disabled.
-  * 
-  * 	@author	: Gabriel D. Goldman
   *
-  * 	@date	: 	01/08/2023 First release
-  * 				    16/09/2024 Last update
+  * @author	: Gabriel D. Goldman
+  * mail <gdgoldman67@hotmail.com>
+  * Github <https://github.com/GabyGold67>
+  *
+  * @date	: 	01/08/2023 First release
+  * 			   16/09/2024 Last update
   *
   ******************************************************************************
-  * @attention	This file is part of the examples folder for the ButtonToSwitch_ESP32
-  * library. All files needed are provided as part of the source code for the library.
+  * @warning **Use of this library is under your own responsibility**
+  * 
+  * @warning The use of this library falls in the category described by The Alan 
+  * Parsons Project (c) 1980 "Games People play" disclaimer:  
+  * Games people play, you take it or you leave it  
+  * Things that they say aren't alright  
+  * If I promised you the moon and the stars, would you believe it?  
   * 
   * Released into the public domain in accordance with "GPL-3.0-or-later" license terms.
-  *
   ******************************************************************************
   */
 #include <Arduino.h>
@@ -50,8 +59,8 @@ void Error_Handler();
 
 const uint8_t dmpbSwitchPin{GPIO_NUM_25};
 const uint8_t dmpbLoadPin{GPIO_NUM_21};
-const uint8_t tvLoadPin{GPIO_NUM_19};
-const uint8_t dmpbIsDisabledPin{GPIO_NUM_18};
+const uint8_t tvLoadPin{GPIO_NUM_18};
+const uint8_t dmpbIsDisabledPin{GPIO_NUM_19};
 
 TimerHandle_t enableSwpTmrHndl{NULL};
 BaseType_t tmrModRslt;
